2e6fd0c23305c62d30d206adb668bbcf268bea2a
eeyll18 <edaeylulgunay2@gmail.com>
2025-02-15 15:42:32 +0300

    add middleware for checking login status

---
+++ middlewares.js
@@ -0,0 +1,15 @@
+exports.checkedLoggedIn = (req,res,next) => {
+    if(req.session.user){
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+exports.byPassLogin = (req,res,next) => {
+    if(!req.session.user){
+        next();
+    }else{
+        res.redirect('/');
+    }
+}
